d223923fb8002d586616c6901ae19008b989ba00
roman.maksymenko <roman.maksymenko@tensquaregames.com>
2020-08-23 13:18:11 +0200

    Finished UI (imported TMPro, added tips panel and level reload)

---
+++ Assets/Scripts/UI/ReloadLevel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ReloadLevel : MonoBehaviour
+{
+    public void ReloadScene(int sceneID)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneID);
+    }
+}
